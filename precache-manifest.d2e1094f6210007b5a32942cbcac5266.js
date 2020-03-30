3732b3472343c361e4f22d98bfd3c8b5cc0a8def
Zakiulhassan <zakihassan555@gmail.com>
2020-03-30 06:33:55 +0500

    Updates

---
+++ precache-manifest.d2e1094f6210007b5a32942cbcac5266.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fed656eb441b3ac55556b0f2cd6697e3",
+    "url": "/little-robots/index.html"
+  },
+  {
+    "revision": "f4bb9fd1e6fcfb9e015b",
+    "url": "/little-robots/static/css/main.28ae7f51.chunk.css"
+  },
+  {
+    "revision": "15ce7e4c935030288850",
+    "url": "/little-robots/static/js/2.93dcc04d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/little-robots/static/js/2.93dcc04d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4bb9fd1e6fcfb9e015b",
+    "url": "/little-robots/static/js/main.5c865c76.chunk.js"
+  },
+  {
+    "revision": "5e16e9a017594ae925f2",
+    "url": "/little-robots/static/js/runtime-main.fcac8b9e.js"
+  }
+]);
